db37145e79c53a4f8e6e85f9bfa4d822a5473363
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-09-18 20:11:36 +0900

    推し曲のプレイリスト追加

--- main.ts
+++ main.ts
@@ -1,11 +1,62 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { App, TerraformStack, TerraformOutput, Token } from "cdktf";
+import { SpotifyProvider, Playlist, DataSpotifySearchTrack } from "./.gen/providers/spotify";
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
+    // アーティスト名
+    const artists = [
+      "Awich",
+      "柊人",
+      "BASI",
+      "SALU",
+      "BAD HOP",
+      "PUNPEE",
+      "VaVa",
+      "BIM",
+      "imase",
+      "KEIJU",
+      "舐達麻",
+      "SPARTA",
+      "604",
+      "Daichi Yamamoto",
+      "KID FRESINO",
+      "OMSB",
+      "kZm"
+    ]
+
+    // データソース
+    // https://www.terraform.io/cdktf/concepts/data-sources#define-data-sources
+    const tracks = artists.map((artist, index) => {
+      const dataSpotifySearchTrack = new DataSpotifySearchTrack(this, `searchTrack_${index}`, {
+        artist: artist,
+      })
+      return [...Array(4).keys()].map(n => dataSpotifySearchTrack.tracks.get(n).id)
+    }).flat()
+
+    // プロバイダー
+    // https://www.terraform.io/cdktf/concepts/providers
+    new SpotifyProvider(this, "spotify", {
+      apiKey: Token.asString(process.env.SPOTIFY_API_KEY),
+    });
+
+    // リソース作成（プレイリスト）
+    const spotify_playlist = new Playlist(this, "playlist", {
+      name: "My Playlist CDKTF",
+      description: "This playlist was created by CDKTF",
+      public: true,
+      tracks,
+    });
+
+    // 出力
+    // https://www.terraform.io/cdktf/concepts/variables-and-outputs
+    new TerraformOutput(this, "playlist url", {
+      value: `https://open.spotify.com/playlist/${spotify_playlist.id}`,
+    });
   }
 }
 
